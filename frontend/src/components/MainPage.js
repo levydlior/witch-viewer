688cf0bf5865f4db82a7c0d030abd4839c75f246
Lior <levydlior@gmail.com>
2022-07-26 18:47:27 -0600

    witch card styling

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -4,7 +4,7 @@ import WitchCard from "./WitchCard";
 
 const WITCH = gql`
   query {
-    tokens(first: 5) {
+    tokens(first: 20) {
       type
       name
       image
@@ -34,18 +34,13 @@ function MainPage() {
 
   console.log(witches);
 
-  if (loading)
-    return (
-      <>
-        <img
-          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db0b76b4-0d59-4a93-9385-4b798b55e81c/ddvmr2p-4953f092-d0bd-49a3-95c0-af632d2acfc0.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RiMGI3NmI0LTBkNTktNGE5My05Mzg1LTRiNzk4YjU1ZTgxY1wvZGR2bXIycC00OTUzZjA5Mi1kMGJkLTQ5YTMtOTVjMC1hZjYzMmQyYWNmYzAuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.6dCKx2Z7afnk1dbKnuuFXfPAiKIgKEZ1OCXDbdYIjcM"
-          alt="flying broom"
-        />
-        <h2>Gathering the witches!</h2>
-      </>
-    );
+  if (loading) return <></>;
 
-  return <div className="App">{witchList}</div>;
+  return (
+    <div className="main-page-witches">
+      {loading ? <h2>Gathering the witches!</h2> : <>{ witchList }</>}
+    </div>
+  );
 }
 
 export default MainPage;
