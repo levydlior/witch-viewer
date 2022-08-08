867c849291d09ffd7287bcd41b2954ed4cbec5fb
Lior <levydlior@gmail.com>
2022-08-08 16:13:52 -0600

    add click on logo push to main page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
 import logo from './../gifs/logo.jpg'
+import { useHistory } from "react-router-dom";
 
 function Header({loggedUser, onLogOut}) {
+  const history = useHistory()
   return (
-    <div id="web-header">
-      <div id="logo-title">
+    <div id="web-header" >
+      <div id="logo-title" onClick={() => history.push('/')}>
         <img id="logo-img" src={logo} alt="Witch flying" />
         <h1>Witch Viewer</h1>
       </div>
