2cf23bd383fc8762ada8ea5f7dcb0ac714f1de16
Lior <levydlior@gmail.com>
2022-08-11 14:38:12 -0600

    dep

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
   delete '/logout', to: 'sessions#destroy'
+
+
+  get '*path', to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
