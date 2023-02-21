da254c66db93067d06da4eda18165a521d8e5a40
Lior <levydlior@gmail.com>
2023-02-21 11:53:19 -0700

    cnnverted sideburger

--- frontend/src/components/SideBurgerMenu/SideBurgerMenu.tsx
+++ frontend/src/components/SideBurgerMenu/SideBurgerMenu.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -8,32 +8,41 @@ import { NavLinks } from "./SideBurgerMenu.styles";
 import { useHistory } from "react-router-dom";
 import { HandleLogOut } from "./SideBurgerMenu.requests";
 import { Link } from "../NavBar/NavBar.styles";
+import { HeaderProps } from "../../GeneralTypes/GeneralTypes";
 
-export const SideBurgerMenu = ({ loggedUser, onLogOut }) => {
+export const SideBurgerMenu = (props: HeaderProps) => {
+  const { loggedUser, onLogOut } = props
   const [state, setState] = React.useState({
     right: false,
   });
 
-  let history = useHistory();
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
+  const toggleDrawer = (anchor: string, open: boolean, event: React.MouseEvent | React.KeyboardEvent) => {
+    if (event.type === "keydown" && isKeyboardEvent(event) && (event.key === "Tab" || event.key === "Shift")) {
       return;
     }
 
     setState({ ...state, [anchor]: open });
   };
 
-  const list = (anchor) => (
+  const isKeyboardEvent = (event: React.MouseEvent | React.KeyboardEvent): event is React.KeyboardEvent => {
+    return "key" in event;
+  }
+
+  const handleSideClose = (e: React.MouseEvent | React.KeyboardEvent, anchor: string) => {
+    if (e && (e.type === "keydown" || e.type === "click")) {
+      toggleDrawer(anchor, false, e);
+    }
+  }
+
+  const list = (anchor: string) => (
+
     <Box
       sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      onClick={(event) => toggleDrawer(anchor, false, event)}
+      onKeyDown={(event) => toggleDrawer(anchor, false, event)}
     >
+
       <List>
         <div>
           {loggedUser ? (
@@ -47,7 +56,7 @@ export const SideBurgerMenu = ({ loggedUser, onLogOut }) => {
               <Link exact to="/popular-witches" activeClassName="selected">
                 Popular Witches
               </Link>
-              <Link to="/" onClick={(e) => HandleLogOut(e, onLogOut, history)}>
+              <Link to="/" onClick={(e) => HandleLogOut(e, onLogOut)}>
                 Logout
               </Link>
             </NavLinks>
@@ -61,14 +70,13 @@ export const SideBurgerMenu = ({ loggedUser, onLogOut }) => {
     <div>
       {["right"].map((anchor) => (
         <React.Fragment key={"right"}>
-          <Button onClick={toggleDrawer(anchor, true)}>
+          <Button onClick={(e) => toggleDrawer(anchor, true, e)}>
             <MenuIcon />
           </Button>
           <Drawer
             anchor={"right"}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
+            open={state["right"]}
+            onClose={(e: React.MouseEvent | React.KeyboardEvent) => handleSideClose(e, anchor)}>
             {list(anchor)}
           </Drawer>
         </React.Fragment>
